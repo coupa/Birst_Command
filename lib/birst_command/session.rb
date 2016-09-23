6cace25b18bb656d3d2a1d3373a62f871101a06d
Aditya Mandavilli <mandavilli@users.noreply.github.com>
2016-09-23 15:10:00 -0700

    Revert "Auth process changed to look for simple password instead of encrypted"

--- lib/birst_command/session.rb
+++ lib/birst_command/session.rb
@@ -39,7 +39,7 @@ module Birst_Command
         cookies: @auth_cookie,
         message: {
           username: @username, 
-          password: @password #decrypt(@password)
+          password: decrypt(@password)
         })
 
       @auth_cookie = response.http.cookies if @auth_cookie.nil?
